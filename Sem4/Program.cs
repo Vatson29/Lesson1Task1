425b80fdfa10fcaa947b577be6a0b8b3f81a40a5
Vladimir <thatiswrong@yandex.ru>
2024-04-19 15:11:41 +0300

    4 problem

---
+++ Sem4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 0)
+Console.WriteLine("Вы ввели отрицательне число ");
+
+int num = 0;
+
+while (num <= a)
+{
+    Console.WriteLine(num);
+    num = num + 2;
+}
