1f0238e502f9fd33e73fc56b2e97a11ed4f90cea
Vladimir <thatiswrong@yandex.ru>
2024-04-19 10:22:44 +0300

    C# 1les done

--- Les1/Program.cs
+++ Les1/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, Vat!");
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// int value = 100;
+
+// Console.WriteLine(value);
+
+// int i1 = 1;
+// int i2 = 2;
+// int sum = i1 + i2;
+// Console.WriteLine(sum);
+
+
+// Console.WriteLine("Введите число 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 3");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 4");
+// int num4 = Convert.ToInt32(Console.ReadLine());
+
+// float res = num1 * num2/(num3 + num4);
+// Console.WriteLine(res);
+
+
+Console.WriteLine("Введите число  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= num1)
+{
+   Console.WriteLine(index);
+   index = index + 1; 
+}
+
+
+
+
+
--- Les1/obj/Debug/net8.0/Les1.AssemblyInfo.cs
+++ Les1/obj/Debug/net8.0/Les1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Les1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f10cdad24ed65f52bf818aef911dd311f96330f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a366d45ebba2a32ee4d1c09365094483fbb56627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Les1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Les1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
