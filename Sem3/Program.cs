8631a1ccecb2ff19296d96dd8d96e92000103b7e
Vladimir <thatiswrong@yandex.ru>
2024-04-19 14:59:59 +0300

    3 probably done? break&

---
+++ Sem3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+
+
+bool b = Convert.ToBoolean (a % 2);
+
+if (b == false)
+{
+    Console.WriteLine("Вы ввели четное число");
+}
+else
+{
+    Console.WriteLine("Вы ввели не четное число");
+}
