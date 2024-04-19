b1acea2fd8e298942da30a0f587e2ecb3759635b
Vladimir <thatiswrong@yandex.ru>
2024-04-19 11:26:47 +0300

    1 sem done

---
+++ Sem1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+Console.WriteLine("Большее число = " + num1 + ", а меньшее =" + num2);    
+//Console.WriteLine("Меньшее " + num2);    
+}
+else
+{
+    Console.WriteLine("Большее число = " + num2 + ", а меньшее =" + num1);    
+}
+
