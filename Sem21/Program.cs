5b2d4ec2f9e970b51f069a81025cc62635366d5b
Vladimir <thatiswrong@yandex.ru>
2024-04-23 10:16:53 +0300

    Homework 21 done

---
+++ Sem21/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("Вы ввели число кратное 7 и 23");
+}
+else
+{
+    Console.WriteLine("Вы ввели число не кратное 7 и 23");
+}
